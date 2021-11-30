0eb71a7fa3d299dd75577ccf1c973afb4a8a7cf1
Vlad1m1ru5 <kubantsev.vov@mail.ru>
2021-11-30 23:44:42 +0300

    go home on login

--- apps/ui/src/app/app.tsx
+++ apps/ui/src/app/app.tsx
@@ -1,15 +1,21 @@
 import { AppBar, Box, Container, CssBaseline } from '@mui/material';
 import { Fragment } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from "react-router-dom";
 import AppCopyright from './app-copyright/app-copyright';
 import AppNavbar from './app-navbar/app-navbar';
 import AppRoutes from './app-routes/app-routes';
 import { AuthFetchStatusDTO, fetchAuth, selectIsAuth } from './auth.slice';
 
 export function App() {
-  const dispatch = useDispatch();
-  const handleLogin = (user: AuthFetchStatusDTO) => dispatch(fetchAuth(user));
+  const history = useHistory();
+  const dispatch = useDispatch();  
   const isAuth = useSelector(selectIsAuth);
+
+  const handleLogin = (user: AuthFetchStatusDTO) => {
+    dispatch(fetchAuth(user));
+    history.push("/");
+  };
 
   return (
     <Fragment>
